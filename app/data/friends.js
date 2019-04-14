84a5041ff55def891c1fed98530ec117a8263bbe
Benham <Benham@Brets-MacBook-Air.local>
2019-04-14 19:08:33 -0400

    updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
 {
     "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"https://i.redd.it/tcqsostuipk21.png",
     "scores":[
         5,
         1,
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,8 +6,30 @@ module.exports = function (app) {
     });
 
     app.post('/api/friends', function (req, res) {
+
+        var newFriend = req.body.scores;
+        var scores = [];
+        var bestMatch = 0;
+
+        for (var i = 0; i < friends.length; i++) {
+            var scoreDifference = 0;
+            for (var j = 0; j < scores.length; j++) {
+                scoreDifference += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(newFriend[j])));
+            }
+            scores.push(scoreDifference);
+        }
+
+        for (var i = 0; i < scores.length; i++) {
+            if (scores[i] <= scores[bestMatch]) {
+                bestMatch = i;
+            }
+        }
+
+        var bestie = friends[bestMatch];
+        res.json(bestie);
+
         friends.push(req.body);
-        // res.json(friends);
+
     });
 }
 
